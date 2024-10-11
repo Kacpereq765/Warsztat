5873ce206bc625d824e674cbd30e09b35889fb0d
k.kalinowski <k.kalinowski@GT-JAS-NB-103>
2024-10-11 09:28:50 +0200

    Wyswietlanie pracwonikow

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Warsztat;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
