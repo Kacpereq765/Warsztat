bdff4c9ffcc75119d78ab0a290137d734cc1a938
k.kalinowski <k.kalinowski@GT-JAS-NB-103>
2024-10-15 12:52:06 +0200

    dodawanie i wyswietlanie samochodow dziala

--- Data/WarsztatDbContext.cs
+++ Data/WarsztatDbContext.cs
@@ -5,11 +5,13 @@ namespace warsztat.Data
 {
     public class warsztatDbContext : DbContext
     {
-        public warsztatDbContext(DbContextOptions<warsztatDbContext> options)
-            : base(options)
-        {
-        }
+        public warsztatDbContext(DbContextOptions<warsztatDbContext> options) : base(options) { }
 
         public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>().ToTable("Cars");
+        }
     }
 }
--- Models/Car.cs
+++ Models/Car.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace warsztat.Models
 {
     public class Car
     {
-        public int Id { get; set; } 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int CarID { get; set; }
 
         [Required(ErrorMessage = "Model is required.")]
-        public string Model { get; set; } 
+        public string Model { get; set; }
 
         [Range(1886, 2100, ErrorMessage = "Year must be between 1886 and 2100.")]
         public int Year { get; set; }
@@ -16,6 +19,6 @@ namespace warsztat.Models
         public bool IsBroken { get; set; }
 
         [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive value.")]
-        public decimal Price { get; set; }
+        public int Price { get; set; }
     }
 }
